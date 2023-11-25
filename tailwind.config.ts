86fe876661ea6428851d18a9e6cf5dd6a406f742
Erik Kjosavik <erik.kjosavik@gmail.com>
2023-11-25 14:41:38 +0100

    Add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,16 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "mp-olive": "#37392eff",
+        "mp-blue": "#19647eff",
+        "mp-turquoise": "#28afb0ff",
+        "mp-gray": "#ddcecdff",
+        "mp-white": "#eee5e5ff",
+      },
+    },
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
 
