5a2d8d2b3bfab14c67942ab84dec28487bcea2e1
Erik Kjosavik <erik.kjosavik@gmail.com>
2023-11-25 15:48:57 +0100

    Add default text color

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="text-mp-olive">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
